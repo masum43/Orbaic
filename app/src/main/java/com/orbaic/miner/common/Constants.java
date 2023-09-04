21b99af269d03059c7c14cc3576ab0479e955b08
masum43 <masum.abir43@gmail.com>
2023-09-04 18:39:37 +0600

    quiz changes

---
+++ app/src/main/java/com/orbaic/miner/common/Constants.java
@@ -0,0 +1,7 @@
+package com.orbaic.miner.common;
+
+public class Constants {
+    public static final String STATE_NOT_STARTED = "not_started";
+    public static final String STATE_STARTED = "started";
+    public static final String STATE_ON_GOING = "on_going";
+}
