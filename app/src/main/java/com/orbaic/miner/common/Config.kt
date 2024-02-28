d0dddd197a4f829c29c3bf339410bccbca879311
masum43 <masum.abir43@gmail.com>
2024-02-28 19:57:58 +0600

    point adding

--- app/src/main/java/com/orbaic/miner/common/Config.kt
+++ app/src/main/java/com/orbaic/miner/common/Config.kt
@@ -1,9 +1,13 @@
 package com.orbaic.miner.common
 
 object Config {
+    var serverAllowedTimeDifference = 30 //minutes
+
     var hourRate = 0.045
+
     var miningRewardedTokenCode = "SHIB"
     var miningRewardedBonusToken: Long = 3000
+
     var quizRewardedTokenCode = "SHIB"
     var quizRewardedBonusToken: Long = 2000
 }
--- app/src/main/java/com/orbaic/miner/common/Constants.java
+++ app/src/main/java/com/orbaic/miner/common/Constants.java
@@ -7,4 +7,13 @@ public class Constants {
 
     public static final String STATUS_ON = "ON";
     public static final String STATUS_OFF = "OFF";
+
+
+    //mining states
+    public static final int STATE_MINING_ERROR = 0;
+    public static final int STATE_MINING_ON_GOING = 1;
+    public static final int STATE_MINING_DATE_DIFF_SERVER = 2;
+    public static final int STATE_MINING_POINTS_GIVEN = 3;
+    public static final int STATE_MINING_POINTS_NOT_GIVEN = 4;
+    public static final int STATE_MINING_FINISHED = 5;
 }
